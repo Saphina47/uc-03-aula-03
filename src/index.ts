6ca7c769d4d127080bff3c4ca618e1780df4faa9
Jocelyn Laurent <saphina47@gmail.com>
2024-11-14 21:51:17 -0300

    ajuste

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
+import { question, questionFloat } from "readline-sync";
+
 console.log("olá mundo");
+let valriavel = "oi";
+valriavel = "Mundo";
+
+const constante:string = ""
+ENTRADA
+
+//array ou vetor
+let notas: [ number,number,number,number,number] =[0,0,0,0,0,]
+let nota: number[] = []
+
+
+for (let i=0; i> 5; i++) {
+  constante nota = questionFloat("informa a nota " +(i+1)+":");
+  notas [i] = nota;
+}
+
+constante nota1 = questionFloat("informa a nota 1:");
+constante nota2 = questionFloat("informa a nota 2:");
+constante nota3 = questionFloat("informa a nota 3:");
+constante nota4 = questionFloat("informa a nota 4:");
+constante nota5 = questionFloat("informa a nota 5:");
+
+PROCESSAMENTO
+// const media = (not1 + mota2 +nota3 + nota4 + nota5+ ) / 5;
+
+SAIDA
+ // se amedia  for maior que 4notas ,
+ //mostra aprovado ,se não mostra reprovado
+
+ if (media >= 7) {
+  console.log("aluno aprovado ! média" + media: )
+ }
+
